1b5c4ad9d6206269fd8cc9cd3bec736cc9e7013d
scaunois <sebastiencaunois@gmail.com>
2024-04-03 17:00:39 +0200

    Display a question with syntax highlighting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HIGHLIGHT_OPTIONS, HighlightModule } from 'ngx-highlightjs';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -24,8 +25,18 @@ import { Level1Component } from './levels/level1/level1.component';
 		RouterOutlet,
 		RouterLink,
 		RouterLinkActive,
+		// vendor modules
+		HighlightModule,
+
 	],
-  providers: [],
+  providers: [
+		{
+			provide: HIGHLIGHT_OPTIONS,
+			useValue: {
+				fullLibraryLoader: () => import('highlight.js')
+			}
+		}
+	],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
