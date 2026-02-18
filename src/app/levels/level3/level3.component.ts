c9dac7e3bd1c214bddfd391b984440924b8725da
scaunois <sebastiencaunois@gmail.com>
2026-02-18 13:46:54 +0100

    Add level 3

--- src/app/levels/level3/level3.component.ts
+++ src/app/levels/level3/level3.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-level3',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./level3.component.css']
 })
 export class Level3Component {
+
+	levelForm: FormGroup;
+
+	constructor(fb: FormBuilder) {
+		this.levelForm = fb.group({
+			q1: '',
+			q2: '',
+			q3: '',
+		});
+	}
+
+	validateAnswers(): void {
+		const fv = this.levelForm.value;
+		const success = fv.q1 === '3' && fv.q2 === '2' && fv.q3 === '1';
+		if (success)
+			alert("quizz completed!")
+		else
+			alert("try again!")
+	}
 
 }
